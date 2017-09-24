0a55b189529bb26979850569ba85fc6cd5cd8fc5
ygy <ygyworkforit@163.com>
2017-09-24 16:22:28 +0800

    leetcode  week4 求最大和的子数组

---
+++ easy/src/main/java/ygy/test/week4/MaximumSubarray.java
@@ -0,0 +1,42 @@
+package ygy.test.week4;
+
+/**
+ * Created by guoyao on 2017/9/24.
+ */
+public class MaximumSubarray {
+
+    public static void main(String[] args) {
+        System.out.println(maxSubArray(new int[]{-2, 1, -3, 4, -1, 2, 1, -5, 4}));
+        System.out.println(maxSubArray_2(new int[]{-2, 1, -3, 4, -1, 2, 1, -5, 4}));
+
+    }
+
+    //{ -1 , -2 , 3 , -4 , 9 , 2,24 ,-12}
+
+    public static int maxSubArray(int[] nums) {
+        int sum=0;
+        int lastSum=Integer.MIN_VALUE;
+        for (int i=0; i < nums.length; i++) {
+            if (sum < 0) {
+                sum=nums[i];
+            } else {
+                sum+=nums[i];
+            }
+            if (sum > lastSum) {
+                lastSum=sum;
+            }
+        }
+        return lastSum;
+    }
+
+    //leetcode
+    public static int maxSubArray_2(int[] nums) {
+        int startNum=nums[0];
+        int goingNum=nums[0];
+        for (int i=1; i < nums.length; i++) {
+            startNum=Math.max(startNum + nums[i], nums[i]);
+            goingNum=Math.max(goingNum, startNum);
+        }
+        return goingNum;
+    }
+}
